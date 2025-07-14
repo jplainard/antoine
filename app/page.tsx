6d57a797ffc36d45108ef32288772f65bcfb1a34
Ton Nom <ton@email.com>
2025-07-14 07:40:49 +0200

    esssai

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function Home() {
+export default function Home( ) {
   return (
     <div className="min-h-screen bg-white">
       {/* Header */}
